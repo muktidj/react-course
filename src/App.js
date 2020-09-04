b616952be24415b338eac54c200e69206f5ada38
muktidj <muktidwijatmoko@gmail.com>
2020-09-04 16:49:30 +0700

    Bab 3, 32. Creating a Functional Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import PersonFunc from './Person/Person';
 
 function App() {
   return (
-    // <div className="App">
-    //   <h1>Hii, From React</h1>
-    // </div>
-    React.createElement('div',{className: 'App'},  React.createElement('h1', 'null', 'Does this work ?'))
+    <div className="App">
+      <h1>Hii, From React</h1>
+      <PersonFunc />
+    </div>
+
   );
 }
 
