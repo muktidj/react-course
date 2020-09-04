d8351aa79812c6a58bd2781d02bc80c8e731aeb4
muktidj <muktidwijatmoko@gmail.com>
2020-09-04 16:26:53 +0700

    Bab 3, 30. React Without JSX

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hii, From React</h1>
-    </div>
+    // <div className="App">
+    //   <h1>Hii, From React</h1>
+    // </div>
+    React.createElement('div',{className: 'App'},  React.createElement('h1', 'null', 'Does this work ?'))
   );
 }
 
