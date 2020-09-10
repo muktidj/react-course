7bb2c255bbae01d6c1264f028e8cf286890f2b81
muktidj <muktidwijatmoko@gmail.com>
2020-09-10 11:50:12 +0700

    bab 3, 35 Output dynamic content {}

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p>This is Functional Component</p>
+    return <p>This is Functional Component. i am {Math.floor(Math.random() * 30)} years old</p>
 }
 
 
