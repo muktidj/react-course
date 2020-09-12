252d32ec688af3ef0da13215f937ed44435ec71f
muktidj <muktidwijatmoko@gmail.com>
2020-09-12 11:22:56 +0700

    bab 3, 38 understanding state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import PersonFunc from './Person/Person';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    persons: [
+      {name:'Mukti', age:Math.floor(Math.random() * 30)},
+      {name:'Dwi', age:Math.floor(Math.random() * 30)},
+      {name:'Jatmoko', age:Math.floor(Math.random() * 30)}
+    ]
+  }
+
+  render() {
+   return (
     <div className="App">
       <h1>Hii, From React</h1>
-      <PersonFunc name="Mukti" age={Math.floor(Math.random() * 30)}/>
-      <PersonFunc name="Dwi" age={Math.floor(Math.random() * 30)}>My Hobbies: Football</PersonFunc>
-      <PersonFunc name="Jatmoko" age={Math.floor(Math.random() * 30)}/>
+      <button>Switch</button>
+      <PersonFunc name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <PersonFunc name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Football</PersonFunc>
+      <PersonFunc name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
 
-  );
+    );
+  }
 }
 
 export default App;
