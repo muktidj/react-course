bc6ed75ea38ad5ef63a813219df0bcff202ffb6a
muktidj <muktidwijatmoko@gmail.com>
2020-10-02 18:01:28 +0700

    bab 5, 74 Working CSS Modules with add Person.module.css & media queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,27 +1,27 @@
 import React from 'react';
 // import Radium from 'radium';
-import styled from 'styled-components'
+//import styled from 'styled-components'
 
-// import './Person.css';
-const StyledDiv = styled.div`
+import classes from './Person.module.css';
+// const StyledDiv = styled.div`
 
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
+//     width: 60%;
+//     margin: 16px auto;
+//     border: 1px solid #eee;
+//     box-shadow: 0 2px 3px #ccc;
+//     padding: 16px;
+//     text-align: center;
 
-    @media (min-width: 500px){
-        width: 450px;
+//     @media (min-width: 500px){
+//         width: 450px;
 
-    &:hover {
-        background-color: lightgreen;
-        color: white;
-        font-weight: bold;
-    }
-    }
-`;
+//     &:hover {
+//         background-color: lightgreen;
+//         color: white;
+//         font-weight: bold;
+//     }
+//     }
+// `;
 
 const person = (props) => {
     // const stylePerson = {
@@ -30,13 +30,15 @@ const person = (props) => {
     //     }
     // }
     return (
-    <StyledDiv>
+    // <StyledDiv>
+    <div className={classes.Person}>
 
         <p onClick={props.click}>Hello, My name is {props.name}. I am {props.age} years old</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
 
-    </StyledDiv>
+    </div>
+    // </StyledDiv>
     )
 }
 
