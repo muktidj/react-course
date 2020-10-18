1c436848ab9eb3375eeab229b242a6fed7beba3f
muktidj <muktidwijatmoko@gmail.com>
2020-10-18 15:53:24 +0700

    Bab 7, 87 class based vs functional

--- src/containers/App.js
+++ src/containers/App.js
@@ -95,6 +95,7 @@ class App extends Component {
 
     <div className="App">
       <Cockpit
+        title={this.props.appTitle}
         showPersons={this.state.showPersons}
         persons={this.state.persons}
         clicked={this.toggleNameHandler}
